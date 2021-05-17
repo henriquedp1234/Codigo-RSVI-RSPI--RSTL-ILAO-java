37d518a744cab78790bcbca3b17eec2089200053
henriquedp1234 <39013985+henriquedp1234@users.noreply.github.com>
2021-05-17 17:54:56 -0300

    Calculo RSTL-ILAO* +variância+média+geração de arquivos Tempo+politicas+aproximação SIR
    Calculo RSTL-ILAO* +variância+média+geração de arquivos Tempo+politicas+aproximação SIR

--- MainSIRIteration2calculaMediaVarianciaAproximação.java
+++ MainSIRIteration2calculaMediaVarianciaAproximação.java
@@ -1,4 +1,4 @@
-Calculo RSTL-ILAO* +variância+média+geração de arquivos Tempo+politicas+aproximação SIR
+//Calculo RSTL-ILAO* +variância+média+geração de arquivos Tempo+politicas+aproximação SIR
 import Problem.Arquivo;
 import Problem.Description.Acao;
 import Problem.Description.Estado;
